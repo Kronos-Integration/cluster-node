7a3ad7f4c34d758bf4a8f002d27106d04e15cbdf
Markus Felten <Markus.Felten@gmx.de>
2015-06-25 19:56:13 +0200

    does not work - credentials are missing

--- update.sh
+++ update.sh
@@ -1,9 +1,12 @@
-
 OWNER=admin
 LAUNCH_CFG=/Library/LaunchDaemons/kronos.plist
 
-su - $OWNER 'cd /Groups/services/kronos;git pull'
-su - $OWNER 'cd /Groups/services/kronos;npm install'
+(
+  cd /Groups/services/kronos
+  su $OWNER -c 'git pull'
+  su $OWNER -c 'npm install'
+)
 
 launchctl unload $LAUNCH_CFG
 launchctl load $LAUNCH_CFG
+
