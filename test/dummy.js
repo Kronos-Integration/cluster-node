54e2c5f250334b26654f5ac07f8d1ff52cef3802
Markus Felten <Markus.Felten@gmx.de>
2015-09-11 10:35:59 +0200

    chore: dummy test added

---
+++ test/dummy.js
@@ -0,0 +1,15 @@
+/* global describe, it, xit */
+/* jslint node: true, esnext: true */
+
+"use strict";
+
+const chai = require('chai');
+const assert = chai.assert;
+const expect = chai.expect;
+const should = chai.should();
+
+describe('dummy', function () {
+  it('dummy', function () {
+    assert.equal(1);
+  });
+});
