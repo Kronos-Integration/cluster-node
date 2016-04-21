ee3b35ba1ff2285c9ee4d5e856724f67da26b38f
Markus Felten <Markus.Felten@gmx.de>
2016-04-21 13:11:59 +0200

    chore(travis): start consul

---
+++ tests/travis/start_consul.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
+pwd
+
+NAME=consul
+VERSION=0.6.4
+ARCHIVE_NAME=${NAME}_${VERSION}_linux_amd64.zip
+
+if [ ! -f ${NAME} ]; then
+  if [ ! -f ${ARCHIVE_NAME} ]; then
+    rm ${ARCHIVE_NAME}
+    curl -O -insecure https://releases.hashicorp.com/${NAME}/${VERSION}/${ARCHIVE_NAME}
+  fi
+
+  rm ${NAME}
+  unzip -o ${ARCHIVE_NAME}
+fi
+
+./consul version
+
+nohup ./consul agent -dev &
