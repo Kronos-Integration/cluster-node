42d113acfd80933ee0c4623cb59a2aa1f3f0c2b6
Markus Felten <Markus.Felten@gmx.de>
2016-04-22 20:44:04 +0200

    test: simple script startup test

--- tests/dummy_test.js
+++ tests/dummy_test.js
@@ -1,15 +1,33 @@
 /* global describe, it, xit */
 /* jslint node: true, esnext: true */
 
-"use strict";
+'use strict';
 
-const chai = require('chai');
-const assert = chai.assert;
-const expect = chai.expect;
-const should = chai.should();
+const chai = require('chai'),
+  assert = chai.assert,
+  expect = chai.expect,
+  should = chai.should(),
+  path = require('path'),
+  child_process = require('child_process');
 
-describe('dummy', function () {
-  it('dummy', function () {
-    assert.equal(1, 1);
+describe('cluster_node', function () {
+  it('simple start', function (done) {
+
+    const child = child_process.exec([
+      path.join(__dirname, '../bin/cluster_node'),
+      '--trace'
+    ].join(' '), (err, stdout, stderr) => {
+      console.log(err);
+      console.log(stdout);
+      //assert.isNull(err);
+      //assert.isTrue(fs.existsSync(path.join(dir, "success.txt")), "did not find success.txt");
+      //assert.match(stdout, /completed/, "Expected a success message");
+      done();
+    });
+
+    setTimeout(() => {
+      console.log(`send signal`);
+      child.kill('SIGKILL');
+    }, 1000);
   });
 });
