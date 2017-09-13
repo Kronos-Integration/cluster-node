94bfc2b579e1da756aba782f4aed2c5650b78a69
Markus Felten <Markus.Felten@gmx.de>
2017-09-13 11:35:28 +0200

    chore(rollup): remove rollup-plugin-multi-source

--- tests/rollup.config.js
+++ tests/rollup.config.js
@@ -1,17 +1,7 @@
-import babel from 'rollup-plugin-babel';
-import multiEntry from 'rollup-plugin-multi-entry';
-
 export default {
-  entry: 'tests/**/*-test.js',
+  entry: 'tests/simple-test.js',
   external: ['ava'],
-  plugins: [
-    babel({
-      babelrc: false,
-      presets: ['stage-3'],
-      exclude: 'node_modules/**'
-    }),
-    multiEntry()
-  ],
+  plugins: [],
   format: 'cjs',
   dest: 'build/test-bundle.js',
   sourceMap: true
