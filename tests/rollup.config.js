914e2cc447a937f79c1c7da3b8bf8b769f77df41
Markus Felten <Markus.Felten@gmx.de>
2018-05-01 09:32:40 +0200

    chore(rollup): generate bin/* by including base rollup.config.js into test config

--- tests/rollup.config.js
+++ tests/rollup.config.js
@@ -1,14 +1,19 @@
-export default {
-  input: 'tests/simple-test.js',
-  external: [
-    'ava',
-    'kronos-service-manager',
-    'config-expander',
-    'npm-package-walker'
-  ],
-  output: {
-    file: 'build/simple-test.js',
-    format: 'cjs',
-    sourcemap: true
+import base from '../rollup.config.js';
+
+export default [
+  base,
+  {
+    input: 'tests/simple-test.js',
+    external: [
+      'ava',
+      'kronos-service-manager',
+      'config-expander',
+      'npm-package-walker'
+    ],
+    output: {
+      file: 'build/simple-test.js',
+      format: 'cjs',
+      sourcemap: true
+    }
   }
-};
+];
