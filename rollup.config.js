20a8e44ebbc5050667a50853632189dece86bc33
Markus Felten <Markus.Felten@gmx.de>
2018-09-13 16:23:57 +0200

    fix(rollup): generate node call with --experimental-modules --experimental-worker

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,25 @@
-import cleanup from 'rollup-plugin-cleanup';
-import pkg from './package.json';
-import json from 'rollup-plugin-json';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import executable from 'rollup-plugin-executable';
+import cleanup from "rollup-plugin-cleanup";
+import pkg from "./package.json";
+import json from "rollup-plugin-json";
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import executable from "rollup-plugin-executable";
 
 export default {
   output: {
-    file: pkg.bin['kronos-cluster-node'],
-    format: 'cjs',
-    banner: '#!/usr/bin/env node'
+    file: pkg.bin["kronos-cluster-node"],
+    format: "cjs",
+    banner: "#!/usr/bin/env node --experimental-modules --experimental-worker",
+    interop: false
   },
-  plugins: [resolve(), commonjs(), json(), executable()],
+  plugins: [resolve(), commonjs(), json(), cleanup(), executable()],
   external: [
-    'os',
-    'child_process',
-    'path',
-    'kronos-service-manager',
-    'config-expander',
-    'npm-package-walker'
+    "os",
+    "child_process",
+    "path",
+    "kronos-service-manager",
+    "config-expander",
+    "npm-package-walker"
   ],
   input: pkg.module
 };
