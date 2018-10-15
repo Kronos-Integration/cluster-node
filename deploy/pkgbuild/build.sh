abc203898d459818d48372ddf00b9f89fd22d878
Markus Felten <Markus.Felten@gmx.de>
2018-10-16 00:04:07 +0200

    fix: deploy aur to nex server

--- deploy/pkgbuild/build.sh
+++ deploy/pkgbuild/build.sh
@@ -4,10 +4,9 @@ rm -rf kronos-cluster-node pkg src custom.db.tar.gz
 
 makepkg -f
 
-REPO=mfelten_de@mfelten.de:/home/mfelten_de/docroot/arch-linux/packages
+REPO=deploy@mfelten.dynv6.net:/data/binaries/arch-linux/packages
 
-scp $REPO/custom.db.tar.gz . 
+scp $REPO/custom.db.tar.gz .
 repo-add custom.db.tar.gz *.xz
-scp *.xz custom.db* $REPO 
+scp *.xz custom.db* $REPO
 rm *.xz
-
