cbaab9747a6289962a6a98edec818e8696f60462
Markus Felten <Markus.Felten@gmx.de>
2019-11-15 12:10:04 +0100

    chore(rollup): sync from template

--- rollup.config.js
+++ rollup.config.js
@@ -1,38 +1,63 @@
-import builtins from "builtin-modules";
-import cleanup from "rollup-plugin-cleanup";
-import pkg from "./package.json";
-import json from "rollup-plugin-json";
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import executable from "rollup-plugin-executable";
+import json from "@rollup/plugin-json";
+import cleanup from "rollup-plugin-cleanup";
+import builtins from "builtin-modules";
+import pkg from "./package.json";
 
-export default {
-  output: {
-    file: pkg.bin["kronos-cluster-node"],
+const external = [
+  ...builtins,
+  "kronos-service-manager",
+  "npm-package-walker",
+  "rebirth",
+  "network-address",
+  "caporal"
+];
+const extensions = ["js", "mjs", "jsx", "tag"];
+const plugins = [
+  commonjs(),
+  resolve(),
+  json({
+    preferConst: true,
+    compact: true
+  }),
+  cleanup({
+    extensions
+  })
+];
+
+const config = Object.keys(pkg.bin || {}).map(name => {
+  return {
+    input: `src/${name}-cli.mjs`,
+    output: {
+      plugins: [executable()],
+      banner:
+        '#!/bin/sh\n":" //# comment; exec /usr/bin/env node --experimental-modules --experimental-wasm-modules "$0" "$@"',
+      file: pkg.bin[name]
+    }
+  };
+});
+
+if (
+  pkg.module !== undefined &&
+  pkg.main !== undefined &&
+  pkg.module != pkg.main
+) {
+  config.push({
+    input: pkg.module,
+    output: {
+      file: pkg.main
+    }
+  });
+}
+
+export default config.map(c => {
+  c.output = {
+    interop: false,
+    externalLiveBindings: false,
     format: "cjs",
-    banner:
-      '#!/bin/sh\n":" //# comment; exec /usr/bin/env node --experimental-modules "$0" "$@"',
-    interop: false
-  },
-  plugins: [
-    commonjs(),
-    json({
-      include: "package.json",
-      preferConst: true,
-      compact: true
-    }),
-    cleanup({
-      extensions: ['js','mjs','jsx','tag']
-    }),
-    executable()
-  ],
-  external: [
-    ...builtins,
-    "kronos-service-manager",
-    "npm-package-walker",
-    "rebirth",
-    "network-address",
-    "caporal"
-  ],
-  input: pkg.module
-};
+    ...c.output
+  };
+  return { plugins, external, ...c };
+});
