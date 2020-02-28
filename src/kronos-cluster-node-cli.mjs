c358f717c9041e4e69481f86e1d6d8f95ae91327
Markus Felten <Markus.Felten@gmx.de>
2020-02-28 18:59:48 +0100

    chore: add missing src/kronos-cluster-node-cli.mjs from template

---
+++ src/kronos-cluster-node-cli.mjs
@@ -0,0 +1,31 @@
+import version from "consts:version";
+import description from "consts:description";
+import { StandaloneServiceProvider } from "@kronos-integration/service";
+import { setup } from "./kronos-cluster-node.mjs";
+
+const args = process.argv.slice(2);
+
+switch (args[0]) {
+  case "--version":
+    console.log(version);
+    process.exit(0);
+  case "--help":
+  case "-h":
+    console.log(`${description} (${version})
+usage:
+ -h --help this help screen
+ -c --config <directory> set config directory`);
+    process.exit(0);
+    break;
+
+  case "--config":
+  case "-c":
+    process.env.CONFIGURATION_DIRECTORY = args[1];
+    break;
+}
+
+try {
+  setup(new StandaloneServiceProvider());
+} catch (error) {
+  console.log(error);
+}
