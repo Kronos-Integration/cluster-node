5b62ff7579f7fe51533e6c49f0d8e7145a640e4e
Markus Felten <Markus.Felten@gmx.de>
2024-01-06 19:28:43 +0100

    add missing src/template-sync-hook-cli.mjs from template

---
+++ src/template-sync-hook-cli.mjs
@@ -0,0 +1,62 @@
+import { readFileSync } from "node:fs";
+import { join } from "node:path";
+import initialize from "./initialize.mjs";
+import { StandaloneServiceProvider } from "@kronos-integration/service";
+
+const args = process.argv.slice(2);
+const opt = { encoding: "utf8" };
+
+switch (args[0]) {
+  case "--version":
+    {
+      const { version } = info();
+      console.log(version);
+      process.exit(0);
+    }
+    break;
+  case "--help":
+  case "-h":
+    {
+      const { description, version } = info();
+      console.log(`${description} (${version});
+usage:
+ -h --help this help screen
+ -c --config <directory> set config directory`);
+      process.exit(0);
+    }
+    break;
+
+  case "--config":
+  case "-c":
+    process.env.CONFIGURATION_DIRECTORY = args[1];
+    break;
+}
+
+initializeServiceProvider();
+
+function info() {
+  return JSON.parse(
+    readFileSync(new URL("../package.json", import.meta.url).pathname, opt)
+  );
+}
+
+async function initializeServiceProvider() {
+  try {
+    let serviceProvider;
+    try {
+      const m = await import("@kronos-integration/service-systemd");
+      serviceProvider = new m.default();
+    } catch (e) {
+      serviceProvider = new StandaloneServiceProvider(
+        JSON.parse(
+          readFileSync(join(args[1], "config.json"), opt)
+        )
+      );
+    }
+
+    await initialize(serviceProvider);
+    await serviceProvider.start();
+  } catch (error) {
+    console.error(error);
+  }
+}
